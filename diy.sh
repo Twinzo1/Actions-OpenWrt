ba1da96d6e5996244fb241ebdbd168729de7c0a7
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2021-04-14 13:16:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/ZZS/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/zzs/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/none/zzs-8888/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
